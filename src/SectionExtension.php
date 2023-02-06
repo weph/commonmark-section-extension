dc086b6e27813105fc90c3397212003ed2d91b08
Philip Weinke <philip.weinke@digitalnoise.de>
2023-02-06 08:58:52 +0100

    Enable psalm's unused code detection

--- src/SectionExtension.php
+++ src/SectionExtension.php
@@ -7,6 +7,9 @@ use League\CommonMark\Environment\EnvironmentBuilderInterface;
 use League\CommonMark\Event\DocumentParsedEvent;
 use League\CommonMark\Extension\ExtensionInterface;
 
+/**
+ * @psalm-suppress UnusedClass
+ */
 final class SectionExtension implements ExtensionInterface
 {
     public function register(EnvironmentBuilderInterface $environment): void
